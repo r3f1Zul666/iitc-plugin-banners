1465fa4fdc32089dd16fabff5741ac46d45355a0
Alexander Urieles <aeurielesn@gmail.com>
2015-11-01 21:43:42 +0100

    Update meta

--- iitc-plugin-banners.user.js
+++ iitc-plugin-banners.user.js
@@ -4,8 +4,8 @@
 // @category       Info
 // @version        0.1.1
 // @namespace      https://github.com/aeurielesn
-// @updateURL      https://github.com/aeurielesn/iitc-plugin-banners/total-conversion-build.meta.js
-// @downloadURL    https://secure.jonatkins.com/iitc/release/total-conversion-build.user.js
+// @updateURL      https://github.com/aeurielesn/iitc-plugin-banners/raw/master/iitc-plugin-banners.meta.js
+// @downloadURL    https://github.com/aeurielesn/iitc-plugin-banners/raw/master/iitc-plugin-banners.user.js
 // @description    Discover mission banners.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*

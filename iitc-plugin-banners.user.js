85e2e5b9f7ed7c86992393c3c80c42bf15d99e07
Alexander Urieles <aeurielesn@gmail.com>
2015-11-01 21:39:54 +0100

    Bump version

--- iitc-plugin-banners.user.js
+++ iitc-plugin-banners.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-banners@aeurielesn
 // @name           IITC plugin: Banners
 // @category       Info
-// @version        0.1.0
+// @version        0.1.1
 // @namespace      https://github.com/aeurielesn
 // @updateURL      https://github.com/aeurielesn/iitc-plugin-banners/total-conversion-build.meta.js
 // @downloadURL    https://secure.jonatkins.com/iitc/release/total-conversion-build.user.js

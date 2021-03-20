67c42910b575d4fbfe383ada2c99300b3c072b8a
Marcel Eggert <m.eggert@heinlein-support.de>
2021-03-20 13:00:55 +0100

    [Change] fix python script

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ def path(provider, directory=""):
 def dpath(provider):
     filename = path(provider, "/" + build_directory)
     dirname = os.path.dirname(os.path.realpath(filename))
-    print dirname
+    print(f"{dirname }")
     if not os.path.exists(dirname):
         os.makedirs(dirname)
     return filename
@@ -24,10 +24,10 @@ def dpath(provider):
 
 def hash(banner):
     with open(path(banner)) as data:
-        contents = data.read()
+        contents = (data.read()).encode('utf-8')
         include = json.loads(contents)
         sha256 = hashlib.sha256(contents).hexdigest()
-        print "%s: %s" % (banner, sha256,)
+        print(f"{banner}: {sha256}")
         shutil.copy(path(banner), dpath("%s$%s" % (banner, sha256)))
 
     return include, sha256
@@ -40,7 +40,7 @@ def generate(provider):
         include = json.load(data)
 
         for subprovider in include.get("providers", []):
-            print "building: %s" % (subprovider,)
+            print(f"building: {subprovider}")
             metadata, sha256, subprovider_length = generate(subprovider)
             include["providers"][subprovider]["name"] = metadata["name"]
             include["providers"][subprovider]["sha256"] = sha256
@@ -48,7 +48,7 @@ def generate(provider):
             provider_length += subprovider_length
 
         for banner in include.get("banners", []):
-            print "hashing: %s" % (banner,)
+            print(f"hashing: {banner}")
             metadata, sha256 = hash(banner)
             include["banners"][banner]["name"] = metadata["name"]
             include["banners"][banner]["authors"] = metadata["authors"]
@@ -61,9 +61,8 @@ def generate(provider):
 
 
 def dump(provider, include):
-    contents = json.dumps(include, indent=2)
+    contents = (json.dumps(include, indent=2)).encode('utf-8')
     sha256 = hashlib.sha256(contents).hexdigest()
-    # print "%s: %s" % (provider, sha256,)
     output_file = dpath("%s$%s" % (provider, sha256))
     with open(output_file, "wb") as f:
         f.write(contents)
@@ -83,7 +82,7 @@ with open("banners.json") as data:
 
     if "providers" in root:
         for provider in root["providers"]:
-            print "building: %s" % (provider,)
+            print(f"building: {provider}")
             metadata, sha256, provider_length = generate(provider)
             root["providers"][provider]["name"] = metadata["name"]
             root["providers"][provider]["sha256"] = sha256
